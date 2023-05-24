db4e53ca15d5ff8b1fa04fed12f822806ebb37ad
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2023-05-24 22:10:52 +0530

    Setup Done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
 
-
-// const name = "Abhishek";
-// const age = 24;
-// const numArr = [1, 2, 3, 4, 5];
-const user = {
-  name: "Abhishek",
-  age:25
-}
 function App() {
   return (
     <div className="App">
-      <p>
-        {/* My name is {name} and age is {age} */}
-        {/* <div>{numArr}</div> */}
-        My name is {user.name} and age is {user.age}
-      </p>
+     <p>Hello World</p>
     </div>
   );
 }
